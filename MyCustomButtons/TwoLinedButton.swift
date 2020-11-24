9ed330ce295cbb626696791d4bc9e81bf6913a76
27293 <myltd6@fpt.com.vn>
2020-11-24 09:52:19 +0700

    first commit

---
+++ MyCustomButtons/TwoLinedButton.swift
@@ -0,0 +1,20 @@
+//
+//  TwoLinedButton.swift
+//  MyCustomButtons
+//
+//  Created by Ngo Dang tan on 23/11/2020.
+//
+
+import UIKit
+
+class TwoLinedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
